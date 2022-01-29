859968d44a4d6b45b1a4996b6bb004b13489b08a
kevinmilly <kevinmilly@gmail.com>
2022-01-28 22:16:23 -0500

    working on sorting table ability

--- src/popup.js
+++ src/popup.js
@@ -26,10 +26,10 @@
     set: (chunkToSave) => {
       chrome.storage.sync.get(["chunks"], function (result) {
         if (!result.chunks || result.chunks.length === 0) {
-          chrome.storage.sync.set({ chunks: [chunkToSave] }, function () {});
+          chrome.storage.sync.set({ chunks: [chunkToSave] }, function () { });
         } else {
           result.chunks.push(chunkToSave);
-          chrome.storage.sync.set({ chunks: result.chunks }, function () {});
+          chrome.storage.sync.set({ chunks: result.chunks }, function () { });
         }
       });
     },
@@ -65,7 +65,7 @@
     taskTab.value = currentTab.url;
 
     restartTable("launchChunkTable");
-    restartTable("viewTable");
+    restartTable("viewTable_body");
     setupLaunchTable(savedChunkNames, savedChunks);
     setUpViewTable(savedChunks);
 
@@ -74,6 +74,7 @@
 
   function setUpViewTable(savedChunksForTable) {
     const table = document.getElementById("viewTable");
+    const tbody = document.getElementById("viewTable_body");
     let checkboxCompletion;
     let td;
     let tr = document.createElement("tr");
@@ -104,7 +105,7 @@
         checkboxCompletion.setAttribute("id", `${chunk.id}`);
         checkboxCompletion.addEventListener("change", completeTask);
         tr.appendChild(checkboxCompletion);
-        table.appendChild(tr);
+        tbody.appendChild(tr);
         tr = document.createElement("tr");
       });
   }
@@ -256,6 +257,54 @@
     }
   };
   //End Tabs functionality
+
+  /**
+ * Sorts a HTML table.
+ * 
+ * @param {HTMLTableElement} table The table to sort
+ * @param {number} column The index of the column to sort
+ * @param {boolean} asc Determines if the sorting will be in ascending
+ */
+  function sortTableByColumn(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
+
+    // Sort each row
+    const sortedRows = rows.sort((a, b) => {
+      const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+      const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+
+      return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+    });
+
+    // Remove all existing TRs from the table
+    while (tBody.firstChild) {
+      tBody.removeChild(tBody.firstChild);
+    }
+
+    // Re-add the newly sorted rows
+    tBody.append(...sortedRows);
+
+    console.log({tBody});
+    console.log({rows});
+
+    // Remember how the column is currently sorted
+    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
+  }
+
+  document.querySelectorAll(".table-sortable th").forEach(headerCell => {
+    headerCell.addEventListener("click", () => {
+      const tableElement = headerCell.parentElement.parentElement.parentElement;
+      const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+      const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+      sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+    });
+  });
+
 
   document.addEventListener("DOMContentLoaded", setupChunks);
 })();
